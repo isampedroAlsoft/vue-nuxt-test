f1662753452366fa779b6a721a8b8b40d8e60bf9
isampedro <isampedro@alsoftllc.onmicrosoft.com>
2023-11-06 11:54:22 -0300

    feat: :sparkles: Added pagination

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    hi
-  </div>
+	<div>
+		<NuxtLayout>
+			<header>
+				<ul>
+					<li>
+						<NuxtLink to="/">Home</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink to="/events">Events</NuxtLink>
+					</li>
+				</ul>
+			</header>
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
 </template>
