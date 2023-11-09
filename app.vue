7a7461715aae6a4e3b0247b58d07663369287dd0
isampedro <isampedro@alsoftllc.onmicrosoft.com>
2023-11-09 16:41:44 -0300

    feat: :sparkles: Bootstrap now works

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+
+<script setup lang="ts">
+const show = ref(false)
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <BButton variant="primary" @click="show = !show">Click me</BButton>
+    <BModal v-model="show">Test</BModal>
   </div>
 </template>
